e66e906d9152b90b0e1797d293b5493cbff0c274
tanaka0s <arima131409@gmail.com>
2022-08-14 19:13:53 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "proposals#index"
-  resources :proposals, only: [:index, :new, :create]
+  resources :proposals, only: [:index, :new, :create] do
+    resources :executions, only: [:index, :new, :create] 
+  end
 end
