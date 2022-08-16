ced73d9504e33b85cf7dc07da7a6d2329b1afd17
tanaka0s <arima131409@gmail.com>
2022-08-16 14:05:20 +0900

    計算フォーム作成

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,88 +1,119 @@
 window.addEventListener("load", ()=> {
-  const path = location.pathname
-  if (path.includes("proposals") && path.includes("new")){
   const beforeSeconds = document.getElementById("before_seconds");
   const beforeWorkers = document.getElementById("before_workers");
   const beforeDays = document.getElementById("before_days");
   const beforeManHours = document.getElementById("before_man_hours");
-  const hideBeforeManHours = document.getElementById("hide_before_man_hours");
+  const showBeforeManHours = document.getElementById("show_before_man_hours");
   const hourlyWage = document.getElementById("hourly_wage");
-  const hideHourlyWage = document.getElementById("hide_hourly_wage");
+  const showHourlyWage = document.getElementById("show_hourly_wage");
   const beforeCosts = document.getElementById("before_costs");
-  const hideBeforeCosts = document.getElementById("hide_before_costs");
+  const showBeforeCosts = document.getElementById("show_before_costs");
   const afterSeconds = document.getElementById("after_seconds");
   const afterWorkers = document.getElementById("after_workers");
   const afterDays = document.getElementById("after_days");
   const afterManHours = document.getElementById("after_man_hours");
-  const hideAfterManHours = document.getElementById("hide_after_man_hours");
+  const showAfterManHours = document.getElementById("show_after_man_hours");
   const afterCosts = document.getElementById("after_costs");
-  const hideAfterCosts = document.getElementById("hide_after_costs");
+  const showAfterCosts = document.getElementById("show_after_costs");
   const reducedManHours = document.getElementById("reduced_man_hours");
-  const hideReducedManHours = document.getElementById("hide_reduced_man_hours");
+  const showReducedManHours = document.getElementById("show_reduced_man_hours");
   const reducedCosts = document.getElementById("reduced_costs");
-  const hideReducedCosts = document.getElementById("hide_reduced_costs");
-  const before_calculation = () => {
-    const beforeManHoursCalc = Math.round((beforeSeconds.value)*(beforeWorkers.value)*(beforeDays.value)*10/3600)/10;
-    beforeManHours.value = beforeManHoursCalc
-    hideBeforeManHours.innerHTML = beforeManHoursCalc
-    const beforeCostsCalc = Math.round((beforeManHours.value)*(hourlyWage.value));
-    beforeCosts.value = beforeCostsCalc
-    hideBeforeCosts.innerHTML = beforeCostsCalc
-    const reducedManHoursCalc = Math.round(((beforeManHours.value)-(afterManHours.value))*10)/10;
-    reducedManHours.value = reducedManHoursCalc
-    hideReducedManHours.innerHTML = reducedManHoursCalc
-    const reducedCostsCalc = Math.round((beforeCosts.value)-(afterCosts.value));
-    reducedCosts.value = reducedCostsCalc
-    hideReducedCosts.innerHTML = reducedCostsCalc
+  const showReducedCosts = document.getElementById("show_reduced_costs");
+
+  if (document.getElementById("identification") != null){
+  // 読み込まれたページに"identification"というid名の要素があれば発火(新規投稿の際)
+    const beforeCalculation = () => {
+      const beforeManHoursCalc = Math.round((beforeSeconds.value)*(beforeWorkers.value)*(beforeDays.value)*10/3600)/10;
+      beforeManHours.value = beforeManHoursCalc
+      showBeforeManHours.innerHTML = beforeManHoursCalc
+      const beforeCostsCalc = Math.round((beforeManHours.value)*(hourlyWage.value));
+      beforeCosts.value = beforeCostsCalc
+      showBeforeCosts.innerHTML = beforeCostsCalc
+      if((beforeSeconds.value != 0) && (beforeWorkers.value != 0) && (beforeDays.value != 0) && (hourlyWage.value != 0) && (afterSeconds.value != 0) && (afterWorkers.value != 0) && (afterDays.value != 0)){
+        const reducedManHoursCalc = Math.round(((beforeManHours.value)-(afterManHours.value))*10)/10;
+        reducedManHours.value = reducedManHoursCalc
+        showReducedManHours.innerHTML = reducedManHoursCalc
+        const reducedCostsCalc = Math.round((beforeCosts.value)-(afterCosts.value));
+        reducedCosts.value = reducedCostsCalc
+        showReducedCosts.innerHTML = reducedCostsCalc
+      };
+    };
+    const afterCalculation = () => {
+      const afterManHoursCalc = Math.round((afterSeconds.value)*(afterWorkers.value)*(afterDays.value)*10/3600)/10;
+      afterManHours.value = afterManHoursCalc
+      showAfterManHours.innerHTML = afterManHoursCalc
+      const afterCostsCalc = Math.round((afterManHours.value)*(hourlyWage.value));
+      afterCosts.value = afterCostsCalc
+      showAfterCosts.innerHTML = afterCostsCalc
+      if((beforeSeconds.value != 0) && (beforeWorkers.value != 0) && (beforeDays.value != 0) && (hourlyWage.value != 0) && (afterSeconds.value != 0) && (afterWorkers.value != 0) && (afterDays.value != 0)){
+        const reducedManHoursCalc = Math.round(((beforeManHours.value)-(afterManHours.value))*10)/10;
+        reducedManHours.value = reducedManHoursCalc
+        showReducedManHours.innerHTML = reducedManHoursCalc
+        const reducedCostsCalc = Math.round((beforeCosts.value)-(afterCosts.value));
+        reducedCosts.value = reducedCostsCalc
+        showReducedCosts.innerHTML = reducedCostsCalc
+      };
+    };
+    beforeSeconds.addEventListener("input",() => {
+      beforeCalculation()
+    });
+    beforeWorkers.addEventListener("input",() => {
+      beforeCalculation()
+    });
+    beforeDays.addEventListener("input",() => {
+      beforeCalculation()
+    });
+    hourlyWage.addEventListener("input",() => {
+      beforeCalculation()
+      showHourlyWage.innerHTML = hourlyWage.value
+      const afterCostsCalc = Math.round((afterManHours.value)*(hourlyWage.value));
+      afterCosts.value = afterCostsCalc
+      showAfterCosts.innerHTML = afterCostsCalc
+    });
+    afterSeconds.addEventListener("input",() => {
+      afterCalculation()
+    });
+    afterWorkers.addEventListener("input",() => {
+      afterCalculation()
+    });
+    afterDays.addEventListener("input",() => {
+      afterCalculation()
+    });
   };
-  const after_calculation = () => {
-    const afterManHoursCalc = Math.round((afterSeconds.value)*(afterWorkers.value)*(afterDays.value)*10/3600)/10;
-    afterManHours.value = afterManHoursCalc
-    hideAfterManHours.innerHTML = afterManHoursCalc
-    const afterCostsCalc = Math.round((afterManHours.value)*(hourlyWage.value));
-    afterCosts.value = afterCostsCalc
-    hideAfterCosts.innerHTML = afterCostsCalc
-    const reducedManHoursCalc = Math.round(((beforeManHours.value)-(afterManHours.value))*10)/10;
-    reducedManHours.value = reducedManHoursCalc
-    hideReducedManHours.innerHTML = reducedManHoursCalc
-    const reducedCostsCalc = Math.round((beforeCosts.value)-(afterCosts.value));
-    reducedCosts.value = reducedCostsCalc
-    hideReducedCosts.innerHTML = reducedCostsCalc
+
+  const path = location.pathname
+  if (path.includes("executions") && path.includes("new")){
+  //読み込まれたパスにexecutionsとnewが含まれていれば発火(改善提案実行の際)
+    let showBeforeManHours = document.getElementById("show_before_man_hours");
+    showBeforeManHours = showBeforeManHours.textContent;
+    let showHourlyWage = document.getElementById("show_hourly_wage");
+    showHourlyWage = showHourlyWage.textContent;
+    let showBeforeCosts = document.getElementById("show_before_costs");
+    showBeforeCosts = showBeforeCosts.textContent;
+    const executionCalculation = () => {
+      const afterManHoursCalc = Math.round((afterSeconds.value)*(afterWorkers.value)*(afterDays.value)*10/3600)/10;
+      afterManHours.value = afterManHoursCalc
+      showAfterManHours.innerHTML = afterManHoursCalc
+      const afterCostsCalc = Math.round((afterManHours.value)*(showHourlyWage));
+      afterCosts.value = afterCostsCalc
+      showAfterCosts.innerHTML = afterCostsCalc
+      if((afterSeconds.value != 0) && (afterWorkers.value != 0) && (afterCosts.value != 0)){
+        const reducedManHoursCalc = Math.round(((showBeforeManHours)-(afterManHours.value))*10)/10;
+        reducedManHours.value = reducedManHoursCalc
+        showReducedManHours.innerHTML = reducedManHoursCalc
+        const reducedCostsCalc = Math.round((showBeforeCosts)-(afterCosts.value));
+        reducedCosts.value = reducedCostsCalc
+        showReducedCosts.innerHTML = reducedCostsCalc
+      };
+    };
+    afterSeconds.addEventListener("input",() => {
+      executionCalculation()
+    });
+    afterWorkers.addEventListener("input",() => {
+      executionCalculation()
+    });
+    afterDays.addEventListener("input",() => {
+      executionCalculation()
+    });
   };
-  beforeSeconds.addEventListener("input",() => {
-    before_calculation ()
-  });
-  beforeWorkers.addEventListener("input",() => {
-    before_calculation ()
-  });
-  beforeDays.addEventListener("input",() => {
-    before_calculation ()
-  });
-  hourlyWage.addEventListener("input",() => {
-    const beforeManHoursCalc = Math.round((beforeSeconds.value)*(beforeWorkers.value)*(beforeDays.value)*10/3600)/10;
-    beforeManHours.value = beforeManHoursCalc
-    const beforeCostsCalc = Math.round((beforeManHours.value)*(hourlyWage.value));
-    beforeCosts.value = beforeCostsCalc
-    hideBeforeCosts.innerHTML = beforeCostsCalc
-    hideHourlyWage.innerHTML = hourlyWage.value
-    const afterCostsCalc = Math.round((afterManHours.value)*(hourlyWage.value));
-    afterCosts.value = afterCostsCalc
-    hideAfterCosts.innerHTML = afterCostsCalc
-    const reducedManHoursCalc = Math.round(((beforeManHours.value)-(afterManHours.value))*10)/10;
-    reducedManHours.value = reducedManHoursCalc
-    hideReducedManHours.innerHTML = reducedManHoursCalc
-    const reducedCostsCalc = Math.round((beforeCosts.value)-(afterCosts.value));
-    reducedCosts.value = reducedCostsCalc
-    hideReducedCosts.innerHTML = reducedCostsCalc
-  });
-  afterSeconds.addEventListener("input",() => {
-    after_calculation ()
-  });
-  afterWorkers.addEventListener("input",() => {
-    after_calculation ()
-  });
-  afterDays.addEventListener("input",() => {
-    after_calculation ()
-  });
- };
 });
