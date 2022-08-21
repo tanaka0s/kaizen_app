a656d52e71c4e614904b5e921024b9d0a68b6e73
tanaka0s <arima131409@gmail.com>
2022-08-21 22:09:49 +0900

    バリデーション修正

--- app/models/execution.rb
+++ app/models/execution.rb
@@ -9,13 +9,19 @@ class Execution < ApplicationRecord
     validates :what
     validates :why
     validates :how
-    validates :after_seconds
-    validates :after_workers
-    validates :after_days
-    validates :hourly_wage
-    validates :after_costs
-    validates :after_man_hours
-    validates :reduced_man_hours
-    validates :reduced_costs
+    validates :after_days, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 365,
+                                         message: "is out of setting range"}
+    with_options numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2_147_483_647,
+                                message: "is out of setting range"} do
+      validates :after_seconds
+      validates :after_workers
+      validates :hourly_wage
+      validates :after_costs
+      validates :reduced_costs
+    end
+    with_options length: { minimum: 1, maximum: 10, message: "is out of setting range" } do
+      validates :after_man_hours
+      validates :reduced_man_hours
+    end
   end
 end
