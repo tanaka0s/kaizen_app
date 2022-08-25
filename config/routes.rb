4e7ebfdf89a100d6fb7897446b35c2f8a72e12db
tanaka0s <arima131409@gmail.com>
2022-08-25 15:18:17 +0900

    モデルとコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "proposals#index"
   resources :proposals, only: [:index, :new, :create, :edit, :update, :destroy] do
-    resources :executions, only: [:new, :create] 
+    resources :executions, only: [:new, :create]
   end
   resources :executions, only: [:index, :edit, :update, :destroy]
 end
