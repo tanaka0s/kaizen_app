9de8b2caf967b2c08a23edf08c8cc532fbd02694
tanaka0s <arima131409@gmail.com>
2022-09-03 17:47:26 +0900

    投稿の編集(実施)　結合テスト

--- spec/system/executions_spec.rb
+++ spec/system/executions_spec.rb
@@ -81,3 +81,119 @@ RSpec.describe '実施済み改善提案の新規投稿', type: :system do
     end
   end
 end
+
+RSpec.describe '実施済み改善提案の編集', type: :system do
+  before do
+    @user1 = FactoryBot.create(:user)
+    @proposal1 = FactoryBot.create(:proposal, user_id: @user1.id)
+    @estimation1 = FactoryBot.create(:estimation, proposal_id: @proposal1.id)
+    @user2 = FactoryBot.create(:user)
+    @proposal2 = FactoryBot.create(:proposal, user_id: @user2.id)
+    @estimation2 = FactoryBot.create(:estimation, proposal_id: @proposal2.id)
+    @execution1 = FactoryBot.create(:execution, proposal_id: @proposal1.id, user_id: @user1.id)
+    @execution2 = FactoryBot.create(:execution, proposal_id: @proposal2.id, user_id: @user2.id)
+  end
+
+  context '実施済み改善提案の投稿内容の編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿した実施済み改善提案を編集できる' do
+      # 実施済み改善提案1を投稿したユーザーでログインする
+      sign_in(@user1)
+      # 実施済み改善提案一覧ページへ遷移する
+      all('.order')[2].click
+      expect(current_path).to eq(executions_path)
+      # 実施済み改善提案1のタイトル部分をクリックしてアコーディオンを開く
+      all('.execution_title_btn')[1].click
+      sleep 0.5
+      # 実施済み改善提案1に編集ページへのリンクがあることを確認する
+      expect(
+        all('.proposal')[1]
+      ).to have_link '編集', href: edit_execution_path(@execution1)
+      # 編集ボタンをクリックすると改善提案1の編集ページへ遷移する
+      all('.action_btn')[1].click
+      expect(current_path).to eq(edit_execution_path(@execution1))
+      # すでに投稿済みの内容が画像以外フォームに入っていることを確認する
+      expect(
+        find('#execution_where').value
+      ).to eq(@execution1.where)
+      expect(
+        find('#execution_what').value
+      ).to eq(@execution1.what)
+      expect(
+        find('#execution_why').value
+      ).to eq(@execution1.why)
+      expect(
+        find('#execution_how').value
+      ).to eq(@execution1.how)
+      expect(
+        find('#after_seconds').value.to_i
+      ).to eq(@execution1.after_seconds)
+      expect(
+        find('#after_workers').value.to_i
+      ).to eq(@execution1.after_workers)
+      expect(
+        find('#after_days').value.to_i
+      ).to eq(@execution1.after_days)
+      expect(
+        find('#after_man_hours', visible: false).value.to_i
+      ).to eq(@execution1.after_man_hours)
+      expect(
+        find('#hourly_wage', visible: false).value.to_i
+      ).to eq(@execution1.hourly_wage)
+      expect(
+        find('#after_costs', visible: false).value.to_i
+      ).to eq(@execution1.after_costs)
+      expect(
+        find('#reduced_man_hours', visible: false).value.to_i
+      ).to eq(@execution1.reduced_man_hours)
+      expect(
+        find('#reduced_costs', visible: false).value.to_i
+      ).to eq(@execution1.reduced_costs)
+      # 投稿内容を編集する
+      image_path = Rails.root.join('public/images/test_image2.png')
+      attach_file('execution[image]', image_path, make_visible: true)
+      fill_in 'execution_where', with: "#{@execution1.where}+編集したテキスト"
+      fill_in 'execution_what', with: "#{@execution1.what}+編集したテキスト"
+      fill_in 'execution_why', with: "#{@execution1.why}+編集したテキスト"
+      fill_in 'execution_how', with: "#{@execution1.how}+編集したテキスト"
+      fill_in 'after_seconds', with: @execution1.after_seconds + 1
+      fill_in 'after_workers', with: @execution1.after_workers + 1
+      fill_in 'after_days', with: @execution1.after_days + 1
+      # 編集してもExecutionモデルのレコードのカウントは変わらないことを確認する
+      expect  do
+        click_on('投稿する')
+      end.not_to change { Execution.count }
+      # トップページに遷移することを確認する
+      expect(current_path).to eq(executions_path)
+      # 実施済み改善提案一覧ページには先ほど投稿した内容が反映されていることを確認する
+      beforeManHours = (@proposal1.before_seconds * @proposal1.before_workers * @proposal1.before_days / 3600).round(1)
+      beforeCosts = (@proposal1.before_seconds * @proposal1.before_workers * @proposal1.before_days / 3600).round(1) * @proposal1.hourly_wage
+      afterManHours = ((@execution1.after_seconds + 1) * (@execution1.after_workers + 1) * (@execution1.after_days + 1) / 3600).round(1)
+      afterCosts = ((@execution1.after_seconds + 1) * (@execution1.after_workers + 1) * (@execution1.after_days + 1) / 3600).round(1) * @execution1.hourly_wage
+      expect(page).to have_selector("img[src$='test_image2.png']")
+      expect(page).to have_content("#{@execution1.where}+編集したテキスト")
+      expect(page).to have_content("#{@execution1.what}+編集したテキスト")
+      expect(page).to have_content("#{@execution1.why}+編集したテキスト")
+      expect(page).to have_content("#{@execution1.how}+編集したテキスト")
+      expect(page).to have_content(@execution1.after_seconds + 1)
+      expect(page).to have_content(@execution1.after_workers + 1)
+      expect(page).to have_content(@execution1.after_days + 1)
+      expect(page).to have_content(afterManHours)
+      expect(page).to have_content(afterCosts)
+      expect(page).to have_content(beforeManHours - afterManHours)
+      expect(page).to have_content(beforeCosts - afterCosts)
+    end
+  end
+  context '実施済み改善提案の投稿内容を編集できないとき' do
+    it 'ログインしたユーザーは自分以外が投稿した実施済み改善提案の編集画面には遷移できない' do
+      # 実施済み改善提案1を投稿したユーザーでログインする
+      sign_in(@user1)
+      # 実施済み改善提案一覧ページへ遷移する
+      all('.order')[2].click
+      expect(current_path).to eq(executions_path)
+      # 実施済み改善提案2に「編集」へのリンクがないことを確認する
+      expect(
+        all('.proposal')[0]
+      ).to have_no_link '編集', href: edit_execution_path(@execution2)
+    end
+  end
+end
